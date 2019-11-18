c60f0ef28262eec1c34d28a2c0c1f841f6f6903b
jota <jose@nubimetrics.com>
2019-11-18 16:31:28 -0300

    renamed

--- src/components/homesForSale/homeForSaleContainer.connect.ts
+++ src/components/homesForSale/homeForSaleContainer.connect.ts
@@ -3,7 +3,7 @@ import { RootState } from "../../reducers/index.reducer";
 import * as actions from "../../actions/index.action";
 import { ActionType, action } from "typesafe-actions";
 import { bindActionCreators } from "redux";
-import CarouselContainer from "./index";
+import { HomeForSaleContainer } from "./homeForSaleContainer";
 
 type Action = ActionType<typeof actions>;
 
@@ -13,8 +13,8 @@ const mapStateToProps = (state: RootState) => ({
     selectFavorite: state.homeforsale.selectFavorite
 });
 
-const mapDispatchToProps = (dispatch: any, props: OwnProps) => bindActionCreators({
-    handleClickFavorite: (data: boolean) => actions.homeSelected(!data),
-}, dispatch);
+// const mapDispatchToProps = (dispatch: any, props: OwnProps) => bindActionCreators({
+//     handleClickFavorite: (data: boolean) => actions.homeSelected(!data),
+// }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CarouselContainer);
+export default connect(mapStateToProps, null)(HomeForSaleContainer);
