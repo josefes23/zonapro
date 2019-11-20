f476bc7641fb1b6a15e62b2edd26418d8033f1fd
jota <jose@nubimetrics.com>
2019-11-20 20:37:24 -0300

    add new action

--- src/actions/index.action.ts
+++ src/actions/index.action.ts
@@ -2,8 +2,10 @@ import { createAction, createAsyncAction } from "typesafe-actions";
 
 import {
 	HOMEFORSALE_SELECT,
-	HOME_PRICE_CHANGE
+	HOME_PRICE_CHANGE,
+	HOME_LOAD_DATA
 } from "../constants/index";
 
 export const homeSelected = createAction(HOMEFORSALE_SELECT, resolve => (select: boolean) => resolve(select))
 export const homePriceChange = createAction(HOME_PRICE_CHANGE, resolve => (price: number) => resolve(price))
+export const homeloadData = createAction(HOME_LOAD_DATA, resolve => (price: number, favorite: boolean) => resolve({ price, favorite }))
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const HOMEFORSALE_SELECT = "@homeforsale/SELECT";
 export const HOME_PRICE_CHANGE = "@price/CHANGE";
+export const HOME_LOAD_DATA = "@price/LOAD";
