cf160d61325e3d1acfe19b9fc9179b0a873bea5f
jota <jose@nubimetrics.com>
2019-11-19 22:30:56 -0300

    call to price action

--- src/components/carousel/carousel.connect.ts
+++ src/components/carousel/carousel.connect.ts
@@ -3,18 +3,20 @@ import { RootState } from "../../reducers/index.reducer";
 import * as actions from "../../actions/index.action";
 import { ActionType, action } from "typesafe-actions";
 import { bindActionCreators } from "redux";
-import CarouselContainer from "./index";
+import CarouselContainer from "./carousel";
 
 type Action = ActionType<typeof actions>;
 
 interface OwnProps { }
 
 const mapStateToProps = (state: RootState) => ({
-    selectFavorite: state.homeforsale.selectFavorite
+    selectFavorite: state.homeforsale.selectFavorite,
+    price: state.homeforsale.price
 });
 
 const mapDispatchToProps = (dispatch: any, props: OwnProps) => bindActionCreators({
     handleClickFavorite: (data: boolean) => actions.homeSelected(!data),
+    handleOnClickEdit: (data: number) => actions.homePriceChange(data),
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(CarouselContainer);
