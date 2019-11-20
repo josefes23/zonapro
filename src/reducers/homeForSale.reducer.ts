16abd3552a7c63310d3be2c17fa8a458f29233d6
jota <jose@nubimetrics.com>
2019-11-19 22:31:35 -0300

    clean reducer

--- src/reducers/homeForSale.reducer.ts
+++ src/reducers/homeForSale.reducer.ts
@@ -5,31 +5,23 @@ type Action = ActionType<typeof actions>;
 
 
 export interface homeForSaleState {
-    selectFavorite: boolean,
-    price: number,
-    showEditButton: boolean
+	selectFavorite: boolean,
+	price: number,
 }
 
 const initState = {
-    selectFavorite: false,
-    price: 0,
-    showEditButton: false
+	selectFavorite: false,
+	price: 0
 }
 
 export const homeforsaleReducer = (state: homeForSaleState = initState, action: Action): homeForSaleState => {
-    switch (action.type) {
-        case getType(actions.homeSelected):
-            return Object.assign({}, state, { selectFavorite: action.payload });
-        case getType(actions.checkPriceChange):
-            return Object.assign({}, state,
-                { showEditButton: action.payload != state.price });
-        case getType(actions.buttonCancel):
-            return Object.assign({}, state,
-                { showEditButton: false });
-        case getType(actions.homePriceChange):
-            return Object.assign({}, state,
-                { price: action.payload, showEditButton: false });
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case getType(actions.homeSelected):
+			return Object.assign({}, state, { selectFavorite: action.payload });
+		case getType(actions.homePriceChange):
+			return Object.assign({}, state,
+				{ price: action.payload });
+		default:
+			return state;
+	}
 }
