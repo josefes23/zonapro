ae61543e19052ca798c74de563167361f8435ec5
jota <jose@nubimetrics.com>
2019-11-20 15:40:07 -0300

    add new components and css clases

--- src/components/HomesForSale/homeForSaleContainer.tsx
+++ src/components/HomesForSale/homeForSaleContainer.tsx
@@ -4,6 +4,7 @@ import CarouselContainer from "../carousel/carousel.connect"
 import EditPriceButton from "../buttons/editable/editPrice.button";
 import { FormatNumber } from "../../services/someServices";
 import HomeBox from "../boxMessage/homeBox";
+import { HomeDetails } from "../lists/homeDetails";
 
 
 
@@ -15,32 +16,30 @@ export class HomeForSaleContainer extends React.Component<any, any>{
 
 	render() {
 		return (
-			<div className={`row ${style.homeForSaleContainer}`}>
-				<section className={`${style.sliderContainer} col-xs-12 col-sm-5`}>
+			<div className={`row-no-gutters ${style.homeForSaleContainer}`}>
+				<section className={`${style.carouselContainer} col-xs-12 col-sm-5`}>
 					<CarouselContainer>
 					</CarouselContainer>
 				</section>
-				<section className="col-xs-12 col-sm-7">
-					<HomeBox/>
-					<div className="homeEditPrice">
+				<section className={`${style.detailContainer} col-xs-12 col-sm-7`}>
+					<HomeBox />
+					<div className="hidden-xs">
 						<EditPriceButton
 							value={this.props.price}
 							handleOnClickEdit={this.props.handleOnClickEdit}
 							formatNumber={(value: number) => FormatNumber(value, 2, "$")} />
 					</div>
-					<div className="row">
-						<div className="col-xs-8">
-							<ul className="list-inline">
-								<li>280 m2</li>
-								<li>3 Dormitorios</li>
-								<li>2 baños</li>
-								<li>2 cocheras</li>
-							</ul>
-						</div>
-						<div className="col-xs-4">
-							<input type="button" className="btn btn-large btn-block btn-default" value="Conectar"></input>
+					<div className={`${style.homeItemsContainer} hidden-xs`}>
+						<div>
+							<div className="col-xs-8">
+								<HomeDetails cssClassCustom={`hidden-xs`} />
+							</div>
+							<div className="col-xs-4">
+								<input type="button" className={`${style.contactButton}`} value="Contactar"></input>
+							</div>
 						</div>
 					</div>
+					<input type="button" className={`${style.contactButton} visible-xs`} value="Contactar"></input>
 				</section>
 			</div>
 		);
