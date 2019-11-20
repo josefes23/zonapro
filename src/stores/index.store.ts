f0470df411fc5e7125bfa58fbdecc7bbe0f70721
jota <jose@nubimetrics.com>
2019-11-20 15:56:40 -0300

    set init price

--- src/stores/index.store.ts
+++ src/stores/index.store.ts
@@ -21,7 +21,7 @@ const composeEnhancers = (
 
 const epicMiddleware = createEpicMiddleware<Action, Action, RootState>();
 
-const initState = { homeforsale: { selectFavorite: false, price: 0 } }
+const initState = { homeforsale: { selectFavorite: false, price: 300000 } }
 
 
 function configureStore(initialState: RootState = initState) {
