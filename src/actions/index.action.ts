dceec9c95148159dda45e67fb3a6ce41e23760c5
jota <jose@nubimetrics.com>
2019-11-19 22:28:07 -0300

    add change price action

--- src/actions/index.action.ts
+++ src/actions/index.action.ts
@@ -2,6 +2,8 @@ import { createAction, createAsyncAction } from "typesafe-actions";
 
 import {
 	HOMEFORSALE_SELECT,
+	HOME_PRICE_CHANGE
 } from "../constants/index";
 
 export const homeSelected = createAction(HOMEFORSALE_SELECT, resolve => (select: boolean) => resolve(select))
+export const homePriceChange = createAction(HOME_PRICE_CHANGE, resolve => (price: number) => resolve(price))
